9741b244db74aab7419ba01aeffa83fbd2df059a
liuziye <liuziye@boswinner.com>
2021-03-25 21:33:07 +0800

    refactor: 💡 修改为我的信息

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `brightzoe's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `brightzoe`,
+      summary: `Learning by doing.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `brightzoe2`,
     },
   },
   plugins: [
